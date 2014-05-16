47bb6bf90f58eb13bb29ee155ee2254afc68860e
AnnaSkachkauskaite <skachkauskaite.anna@gmail.com>
2014-05-16 22:40:56 +0400

    Tests fixed in 2.2

--- Homeworks. Second term/2/2/writertest.h
+++ Homeworks. Second term/2/2/writertest.h
@@ -21,10 +21,8 @@ private slots:
         arr[0] = new int[3] {1, 2, 3};
         arr[1] = new int[3] {4, 5, 6};
         arr[2] = new int[3] {7, 8, 9};
-        string result = writer->makeString(arr, 3); //"5 4 7 8 9 6 3 2 1 ";
-        cout << result << "\n";
+        string result = writer->makeString(arr, 3);
         string out = "5 4 7 8 9 6 3 2 1 ";
-        cout << out;
         for (int i = 0; i < out.size(); ++i)
             QCOMPARE(result[i], out[i]);
 
